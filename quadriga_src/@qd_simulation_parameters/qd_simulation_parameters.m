c88d7b1576f2fefaa2e14a350c2668f69c891d8c
Stephan Jaeckel <jaeckel@sjc-wireless.com>
2025-02-24 18:16:44 +0100

    Set version number

--- quadriga_src/@qd_simulation_parameters/qd_simulation_parameters.m
+++ quadriga_src/@qd_simulation_parameters/qd_simulation_parameters.m
@@ -101,7 +101,7 @@ end
 
 properties(Constant)
     % Version number of the current QuaDRiGa release (constant)
-    version = '3.1.5-0';
+    version = '3.1.6-0';
 end
 
 properties(SetAccess=protected)
